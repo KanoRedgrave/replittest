a06e95a0f2f5ef38939491a4b8ec0816468c5f8a
DarrellHilton <21563123-DarrellHilton@users.noreply.replit.com>
2023-05-05 22:25:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+def main():
+  #Introduction
+    intro()
+
+    try:
+      #Input miles driven
+        num_miles = int(input("Please enter total miles driven: ")) 
+      
+      #Converting miles to kilometers
+        miles_to_kilometers(num_miles) 
+
+    except:
+        print()
+        print("We only accept numbers here, my dude. Try again") 
+        print()
+        main()
+
+  #Intro function displays on the intro screen
+def intro():
+    print("This will convert miles driven to kilometers.")
+    print("For example: 1 miles = 1.60934 kilometers.")
+    print()
+
+  # miles_to_kilometers acccepts the number of miles driven and converts to kilometers
+def miles_to_kilometers(num_miles):
+    kilometers = num_miles * 1.60934
+    print("Total distance driven:", kilometers,"kilometers")
+
+  # calls the main function
+main()
